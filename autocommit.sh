9e723fc99867368e06fb35e1a4aaa3d6ea9e08ec
tastyratz <tastyratz@hotmail.com>
2023-11-10 12:34:37 -0500

    Autocommit from 2023-11-10 12:34:37
    Klipper on commit: 8ef0f7d
    
    Moonraker on commit: 3d9052d
    
    Mainsail version: v2.7.1
    
    Fluidd version: v1.25.3

--- autocommit.sh
+++ autocommit.sh
@@ -76,7 +76,7 @@ push_config(){
   git pull origin $branch --no-rebase
   git add .
   current_date=$(date +"%Y-%m-%d %T")
-  git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
+  git commit -am "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
   git push origin $branch
 }
 
